2ac5f51a3c184661b297d08c52c3c210a30e7560
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:04:09 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan1/biodata/Biodata.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Biodata
+ */
+public class Biodata {
+    public static void main(String[] args){
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t: Steven Danesswaralay");
+        System.out.println("ALAMAT\t\t: Tubagus Ismail XV No.1A");
+        System.out.println("TEMPAT LAHIR\t: Cilegon");
+        System.out.println("TANGGAL LAHIR\t: 04 September 2000");
+        System.out.println("-----------------");
+        System.out.println("HOBI\t\t: Olahraga Basket");
+        System.out.println("MAKES\t\t: Udang Rica rica");
+        System.out.println("MIKES\t\t: Jus Jambu Merah");
+    }
+}
